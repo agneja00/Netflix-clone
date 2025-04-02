4ed2433393ebb9bdab96f740f0441d00da8ecd69
agneja00 <agnieshaa00@gmail.com>
2025-04-02 04:32:39 +0300

    added some fixes for the deployment

--- NetflixClone/vite.config.ts
+++ NetflixClone/vite.config.ts
@@ -1,18 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "./src") },
+      { find: "@styles", replacement: path.resolve(__dirname, "./src/styles") }
+    ],
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/variables";`,
+        additionalData: `@use "@styles/variables" as *;`,
       },
     },
   },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react';
+            if (id.includes('@tanstack')) return 'vendor-react-query';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
