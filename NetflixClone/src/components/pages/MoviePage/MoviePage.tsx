03118a7062820c7ae2649bc5d408deff121523fe
agneja00 <agnieshaa00@gmail.com>
2025-04-23 20:48:43 +0300

    some changes with the design for a better look

--- NetflixClone/src/components/pages/MoviePage/MoviePage.tsx
+++ NetflixClone/src/components/pages/MoviePage/MoviePage.tsx
@@ -6,7 +6,6 @@ import { useMovieDetails } from "../../hooks/hooks";
 const MoviePage = () => {
   const { id } = useParams<{ id: string }>();
   const { data: movie, isLoading, error } = useMovieDetails(id);
-  console.log(movie);
 
   if (isLoading) return <div className={styles.loading}>Loading movie...</div>;
   if (error || !movie)
@@ -15,7 +14,7 @@ const MoviePage = () => {
   const trailer = movie.videos?.results?.find((v) => v.type === "Trailer");
 
   return (
-    <div className={styles.moviePage}>
+    <>
       <div className={styles.hero}>
         <img
           src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
@@ -24,51 +23,52 @@ const MoviePage = () => {
         />
         <h1 className={styles.hero__title}>{movie.title || movie.name}</h1>
       </div>
+      <div className={styles.moviePage}>
+        <div className={styles.moviePage__content}>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt={movie.title}
+            className={styles.moviePage__poster}
+          />
 
-      <div className={styles.moviePage__content}>
-        <img
-          src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-          alt={movie.title}
-          className={styles.moviePage__poster}
-        />
+          <div className={styles.moviePage__details}>
+            <h2 className={styles.moviePage__details__title}>Overview</h2>
+            <p className={styles.moviePage__details__paragraph}>
+              {movie.overview}
+            </p>
 
-        <div className={styles.moviePage__details}>
-          <h2 className={styles.moviePage__details__title}>Overview</h2>
-          <p className={styles.moviePage__details__paragraph}>
-            {movie.overview}
-          </p>
-
-          {movie.genres && (
-            <>
-              <h3 className={styles.moviePage__details__title}>Genres</h3>
-              <ul className={styles.moviePage__details__list}>
-                {movie.genres.map((genre) => (
-                  <li
-                    key={genre.id}
-                    className={styles.moviePage__details__list__link}
-                  >
-                    {genre.name}
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-          {trailer && (
-            <div className={styles.trailer}>
-              <h3 className={styles.moviePage__details__title}>Trailer</h3>
-              <YouTube
-                videoId={trailer.key}
-                opts={{
-                  height: "390",
-                  width: "100%",
-                  playerVars: { autoplay: 0 },
-                }}
-              />
-            </div>
-          )}
+            {movie.genres && (
+              <>
+                <h3 className={styles.moviePage__details__title}>Genres</h3>
+                <ul className={styles.moviePage__details__list}>
+                  {movie.genres.map((genre) => (
+                    <li
+                      key={genre.id}
+                      className={styles.moviePage__details__list__link}
+                    >
+                      {genre.name}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+            {trailer && (
+              <div className={styles.trailer}>
+                <h3 className={styles.moviePage__details__title}>Trailer</h3>
+                <YouTube
+                  videoId={trailer.key}
+                  opts={{
+                    height: "390",
+                    width: "100%",
+                    playerVars: { autoplay: 0 },
+                  }}
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
