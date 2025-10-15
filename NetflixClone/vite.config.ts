45a7a0bc336117cfcbc42e6acd0c669012fe2daa
agneja00 <agnieshaa00@gmail.com>
2025-10-15 17:51:33 +0300

    Added automatic CSS prefix substitution

--- NetflixClone/vite.config.ts
+++ NetflixClone/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 import tsconfigPaths from "vite-tsconfig-paths";
 import legacy from "@vitejs/plugin-legacy";
 
@@ -21,6 +22,9 @@ export default defineConfig(({ mode }) => {
       alias: [{ find: "@", replacement: "/src" }],
     },
     css: {
+      postcss: {
+        plugins: [autoprefixer({})],
+      },
       preprocessorOptions: {
         scss: {
           additionalData: `@use "@/styles/variables" as *;`,
