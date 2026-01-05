4b1e05888d755fb780810103bb03ef69ad18a031
agneja00 <agnieshaa00@gmail.com>
2026-01-05 05:02:48 +0200

    Added footer copyright

---
+++ NetflixClone/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <p className={styles.footer}>
+      Copyright &#169; 2026 Agnieska Jackevic. All Rights Reserved.
+    </p>
+  );
+};
+
+export default Footer;
--- NetflixClone/src/components/layout/RootLayout.tsx
+++ NetflixClone/src/components/layout/RootLayout.tsx
@@ -1,14 +1,16 @@
 import styles from "./RootLayout.module.scss";
 import { Outlet } from "react-router-dom";
 import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
 export const RootLayout = () => {
   return (
-    <>
+    <div className={styles.root}>
       <Nav />
-      <main className={styles.root}>
+      <main>
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 };
